148e1ba8d7a034a9ef35224618cbca330589e3dd
Dmitriy Dubson <d.dubson@gmail.com>
2019-02-05 22:06:21 -0500

    Upgrade Babel and preset

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['env']
+                    presets: ['@babel/env']
                 }
             },
             {
